09ebab173152f6fba020fb684f44c01a3829f143
Gonzalo Albornoz <galbornoz@sgfintech.com.ar>
2025-09-19 09:06:02 -0300

    elimino comentario

--- OrdersApi/FakeService.cs
+++ OrdersApi/FakeService.cs
@@ -12,7 +12,7 @@
         public Task SendNotification(Guid OrderId)
         {
             return Task.Run(() => {
-                Thread.Sleep(2000); // Simulate some work
+                Thread.Sleep(2000);
                 Console.WriteLine($"Notification sent by #{counter} instance. OrderId ({OrderId})");
             });
         }
